856214cb3cdc92cfb8aaee0623b2e8382665bbd3
Plurmp <59399112+Plurmp@users.noreply.github.com>
2022-11-29 23:10:25 -0600

    working final

--- maze.js
+++ maze.js
@@ -3,13 +3,20 @@ let width;
 let height;
 
 const find = (arr, x) => {
-	console.log(x);
-	if (arr[x] < 0) {
+	if (arr[x][0] < 0) {
 		return x;
 	}
 	else {
-		return find(arr, arr[x]);
+		return find(arr, arr[x][0]);
 	}
+}
+
+const union = (a, b) => {
+	const aParent = find(mazeSet, a);
+	const bParent = find(mazeSet, b);
+	if (aParent === bParent) { return; }
+	let aParentCell = mazeSet[aParent]
+	mazeSet[aParent] = [bParent, aParentCell[1]]
 }
 
 const generateMaze = () => {
@@ -45,94 +52,104 @@ const generateMaze = () => {
 	table.appendChild(tbody);
 	document.getElementById("maze-container").appendChild(table);
 
-	mazeSet = new Array(width * height).fill(-1);
-	while (true) {
-		let rootCounter = 0
-		for (let i = 0; i < mazeSet.length; i++) {
-			if (mazeSet[i] === -1) { rootCounter++; }
-			if (rootCounter > 1) { break; }
-		}
-		if (rootCounter === 1) { break; }
-
+	mazeSet = Array.from({length: width * height}, () => [-1, new Set()]);
+	while (find(mazeSet, 0) !== find(mazeSet, mazeSet.length - 1)) {
 		const curCellInd = Math.floor(Math.random() * (mazeSet.length - 1)) + 1; // random number from 1 to (length - 1)
 		const destroyWall = Math.floor(Math.random() * 4)
-		switch (destroyWall) {
-			case 0: { // top wall
-				if (curCellInd < width) { break; }
-				const partnerCellInd = curCellInd - width;
-				console.log(mazeSet + " top");
-				if (find(mazeSet, curCellInd) === find(mazeSet, partnerCellInd)) { break; }
-				mazeSet[curCellInd] = partnerCellInd;
-				const curCell = document.getElementById("cell-" + curCellInd);
-				curCell.style.borderTop = "none";
-				const partnerCell = document.getElementById("cell-" + partnerCellInd);
-				partnerCell.style.borderBottom = "none";
-				break;
-			}
-			case 1: {// right wall
-				if (curCellInd % width === width - 1) { break; }
-				const partnerCellInd = curCellInd + 1;
-				console.log(mazeSet + " right");
-				if (find(mazeSet, curCellInd) === find(mazeSet, partnerCellInd)) { break; }
-				mazeSet[curCellInd] = partnerCellInd;
-				const curCell = document.getElementById("cell-" + curCellInd);
-				curCell.style.borderRight = "none";
-				const partnerCell = document.getElementById("cell-" + partnerCellInd);
-				partnerCell.style.borderLeft = "none";
-				break;
-			}
-			case 2: { // bottom wall
-				if (curCellInd >= mazeSet.length - width) { break; }
-				const partnerCellInd = curCellInd + width;
-				console.log(mazeSet + " bottom");
-				if (find(mazeSet, curCellInd) === find(mazeSet, partnerCellInd)) { break; }
-				mazeSet[curCellInd] = partnerCellInd;
-				const curCell = document.getElementById("cell-" + curCellInd);
-				curCell.style.borderBottom = "none";
-				const partnerCell = document.getElementById("cell-" + partnerCellInd);
-				partnerCell.style.borderTop = "none";
-				break;
-			}
-			case 3: { // left wall
-				if (curCellInd % width === 0) { break; }
-				const partnerCellInd = curCellInd - 1;
-				console.log(mazeSet + " left");
-				if (find(mazeSet, curCellInd) === find(mazeSet, partnerCellInd)) { break; }
-				mazeSet[curCellInd] = partnerCellInd;
-				const curCell = document.getElementById("cell-" + curCellInd);
-				curCell.style.borderLeft = "none";
-				const partnerCell = document.getElementById("cell-" + partnerCellInd);
-				partnerCell.style.borderRight = "none";
-				break;
-			}
+		if (destroyWall === 0) { // top wall
+			if (curCellInd < width) { continue; }
+			const partnerCellInd = curCellInd - width;
+			if (find(mazeSet, curCellInd) === find(mazeSet, partnerCellInd)) { continue; }
+			union(curCellInd, partnerCellInd);
+			const curCell = document.getElementById("cell-" + curCellInd);
+			curCell.style.borderTop = "none";
+			const partnerCell = document.getElementById("cell-" + partnerCellInd);
+			partnerCell.style.borderBottom = "none";
+
+			mazeSet[curCellInd][1].add(partnerCellInd);
+			mazeSet[partnerCellInd][1].add(curCellInd);
+
+		}
+		else if (destroyWall === 1) {// right wall
+			if (curCellInd % width === width - 1) { continue; }
+			const partnerCellInd = curCellInd + 1;
+			if (find(mazeSet, curCellInd) === find(mazeSet, partnerCellInd)) { continue; }
+			union(curCellInd, partnerCellInd);
+			const curCell = document.getElementById("cell-" + curCellInd);
+			curCell.style.borderRight = "none";
+			const partnerCell = document.getElementById("cell-" + partnerCellInd);
+			partnerCell.style.borderLeft = "none";
+
+			mazeSet[curCellInd][1].add(partnerCellInd);
+			mazeSet[partnerCellInd][1].add(curCellInd);
+		}
+		else if (destroyWall === 2) { // bottom wall
+			if (curCellInd >= mazeSet.length - width) { continue; }
+			const partnerCellInd = curCellInd + width;
+			if (find(mazeSet, curCellInd) === find(mazeSet, partnerCellInd)) { continue; }
+			union(curCellInd, partnerCellInd);
+			const curCell = document.getElementById("cell-" + curCellInd);
+			curCell.style.borderBottom = "none";
+			const partnerCell = document.getElementById("cell-" + partnerCellInd);
+			partnerCell.style.borderTop = "none";
+
+			mazeSet[curCellInd][1].add(partnerCellInd);
+			mazeSet[partnerCellInd][1].add(curCellInd);
+		}
+		else if (destroyWall === 3) { // left wall
+			if (curCellInd % width === 0) { continue; }
+			const partnerCellInd = curCellInd - 1;
+			if (find(mazeSet, curCellInd) === find(mazeSet, partnerCellInd)) { continue; }
+			union(curCellInd, partnerCellInd);
+			const curCell = document.getElementById("cell-" + curCellInd);
+			curCell.style.borderLeft = "none";
+			const partnerCell = document.getElementById("cell-" + partnerCellInd);
+			partnerCell.style.borderRight = "none";
+
+			mazeSet[curCellInd][1].add(partnerCellInd);
+			mazeSet[partnerCellInd][1].add(curCellInd);
 		}
 	}
 	document.getElementById("solve-maze").hidden = false;
 }
 
+const search = () => { // depth-first search
+	let stack = [[0, [0]]];
+	let visited = new Array(width * height).fill(false);
+	while (stack.length > 0) {
+		const [curNode, path] = stack.pop();
+		if (!visited[curNode]){
+			if (curNode === width * height - 1) return path;
+			visited[curNode] = true;
+			for (let i of mazeSet[curNode][1]) {
+				stack.push([i, path.concat([i])]);
+			}
+		}
+	}
+}
+
 const solveMaze = () => {
 	document.getElementById("solve-maze").hidden = true;
-	let curCellInd = mazeSet.length - 1;
+
+	const finishedPath = search();
+	console.log(finishedPath);
+
 	let path = "";
-	// build the path from bottom to top, then reverse it
-	while (curCellInd > 0) {
-		document.getElementById("cell-" + curCellInd).style.backgroundColor = "#000";
-		if (mazeSet[curCellInd] === curCellInd - width) {
-			path += "S";
-		}
-		else if (mazeSet[curCellInd] === curCellInd - 1) {
-			path += "E";
-		}
-		else if (mazeSet[curCellInd] === curCellInd + width) {
+	for (let i = 0; i < finishedPath.length; i++) {
+		document.getElementById("cell-" + finishedPath[i]).style.backgroundColor = "#000";
+		if (finishedPath[i + 1] === finishedPath[i] - width) {
 			path += "N";
 		}
-		else if (mazeSet[curCellInd] === curCellInd + 1) {
+		else if (finishedPath[i + 1] === finishedPath[i] - 1) {
 			path += "W";
 		}
-		curCellInd = mazeSet[curCellInd];
+		else if (finishedPath[i + 1] === finishedPath[i] + width) {
+			path += "S";
+		}
+		else if (finishedPath[i + 1] === finishedPath[i] + 1) {
+			path += "E";
+		}
 	}
-	document.getElementById("cell-0").style.backgroundColor = "#000";
-	path = path.split("").reverse().join("");
 	const solution = document.getElementById("maze-solution");
 	solution.innerText = path;
 	solution.hidden = false;
